9d76813eee9ae482e263a1979d88db1ab019f3fb
laerciosouzaDev <laerciogplay@gmail.com>
2024-10-15 21:29:19 -0300

    basicao

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
-alert("Script conectado a página")
+document.getElementById("formularioVeiculo").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    //preventDefault durante qualquer fase do fluxo de eventos cancela o evento,
+    //o que significa que qualquer ação padrão normalmente feita pela aplicação 
+    //como um resultado do evento não ocorrerá
+
+    
+    const marca = document.getElementById("marca").value;
+    const modelo = document.getElementById("modelo").value;
+    const ano = document.getElementById("ano").value;
+
+    
+    const novoVeiculo = document.createElement("li");
+    novoVeiculo.textContent = `Marca: ${marca}, Modelo: ${modelo}, Ano: ${ano}`;
+
+    const img = document.createElement("img")
+    img.src = `img: ${img}`;
+
+    
+    document.getElementById("listaVeiculo").appendChild(novoVeiculo);
+
+    
+    document.getElementById("formularioVeiculo").reset();
+});
